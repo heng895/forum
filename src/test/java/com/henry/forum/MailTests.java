0cb87ab73bfb9d940ae2eb2407c636874738e2b0
heng895 <henry895260@gmail.com>
2023-06-26 17:44:34 +0800

    邮件功能

---
+++ src/test/java/com/henry/forum/MailTests.java
@@ -0,0 +1,42 @@
+package com.henry.forum;
+
+import com.henry.forum.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = ForumApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("2530439948@qq.com", "TEST", "Welcome.");
+    }
+
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("2530439948@qq.com", "HTML", content);
+    }
+
+}
